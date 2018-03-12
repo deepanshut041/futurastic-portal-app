be45455da64967e30549545e8b0f635bff45d882
deepanshut041 <deepanshut041@gmail.com>
2018-03-13 00:53:29 +0530

    Working on mvp making splash screen and api for login and signup

--- app/src/main/java/com/futurastic/deepanshu/portal/ui/main/MainActivity.java
+++ app/src/main/java/com/futurastic/deepanshu/portal/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.futurastic.deepanshu.portal;
+package com.futurastic.deepanshu.portal.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.futurastic.deepanshu.portal.R;
 
 public class MainActivity extends AppCompatActivity {
 
